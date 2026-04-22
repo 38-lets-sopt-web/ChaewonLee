c543838728d892a3dc49eb9412d6b6ea5102bade
chaewon41 <chaew0m3x@gmail.com>
2026-04-22 12:42:30 +0900

    feat: 초기 데이터 저장 로직 구현

--- Week2/Homework/account-book/js/main.js
+++ Week2/Homework/account-book/js/main.js
@@ -1,6 +1,20 @@
-const icon = document.getElementById("header__icon");
+import { expenses } from './data.js';
+
+/* 초기 데이터 저장하기 */
+const initData = () => {
+  if (!localStorage.getItem("expenseData")) {
+    localStorage.setItem("expenseData", JSON.stringify(expenses));
+  }
+};
+
+/* 데이터 불러오기 */
+const getExpenseData = () => {
+  return JSON.parse(localStorage.getItem("expenseData")) || [];
+};
 
 /* 헤더 아이콘 클릭 시 새로고침 */
+const icon = document.getElementById("header__icon");
+
 icon.addEventListener("click", () => {
   location.reload();
 });
