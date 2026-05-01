6a0ede3ca8145ed03058a2dee31b077b893a0f6b
chaewon41 <chaew0m3x@gmail.com>
2026-05-01 15:59:08 +0900

    feat: Header 컴포넌트 레이아웃 및 스타일 구현

--- week3/Homework/my-app/src/App.jsx
+++ week3/Homework/my-app/src/App.jsx
@@ -1,19 +1,15 @@
-import styled from '@emotion/styled'
-
-const Button = styled.button`
-  padding: 10px 20px;
-  background-color: hotpink;
-  font-size: 24px;
-  border: none;
-  color: white;
-  font-weight: bold;
-  &:hover {
-    color: black;
-  }
-`
+import { ThemeProvider } from '@emotion/react'
+import GlobalStyles from './styles/GlobalStyles.jsx'
+import { theme } from './styles/theme.js'
+import Header from './components/Header.jsx'
 
 function App() {
-  return <Button>Emotion 버튼 테스트</Button>
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Header />
+    </ThemeProvider>
+  )
 }
 
 export default App
---
+++ week3/Homework/my-app/src/components/Header.jsx
@@ -0,0 +1,47 @@
+import styled from '@emotion/styled'
+
+export default function Header () {
+    return (
+        <HeaderContainer>
+            <Title>두더지 게임</Title>
+            <Tabs>
+                <Tab>게임</Tab>
+                <Tab>랭킹</Tab>
+            </Tabs>
+        </HeaderContainer>
+
+    )
+
+}
+
+const HeaderContainer = styled.header `
+    display: flex;
+    align-items: center;
+    padding: 16px;
+    gap: 16px;
+    border-radius: ${({theme}) => theme.borderRadius.md};
+    background-color: ${({ theme }) => theme.colors.bgCard};
+`
+
+const Title = styled.h1 `
+    font-size: ${({theme}) => theme.typography.size.title};
+    font-weight: ${({theme}) => theme.typography.weight.bold};
+    color: ${({theme}) => theme.colors.textMain};
+`
+
+const Tabs = styled.div `
+    display: flex;
+    gap: 12px;
+`
+
+const Tab = styled.button `
+    padding: 4px 16px;
+    font-size: ${({theme}) => theme.typography.size.body};
+    font-weight: ${({theme}) => theme.typography.weight.regular};
+    color: ${({theme}) => theme.colors.textMain};
+    border: 1.5px solid ${({ theme }) => theme.colors.secondary};
+    border-radius: ${({theme}) => theme.borderRadius.md};
+    &:hover {
+        background-color: ${({ theme }) => theme.colors.secondary};
+    }
+`
