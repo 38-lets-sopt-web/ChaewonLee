664459718aff648cffd644189c99cb952a73a888
chaewon41 <chaew0m3x@gmail.com>
2026-05-07 20:00:50 +0900

    feat: 회원가입 페이지 아이디 입력 UI 구현

--- week4/Homework/week4-homework/src/pages/Signup.tsx
+++ week4/Homework/week4-homework/src/pages/Signup.tsx
@@ -1,5 +1,25 @@
+import {useState} from 'react'
+import { wrapper } from './Signup.css'
+import Button from '../components/Button'
+import Input from '../components/Input'
+
 export default function Signup() {
+    const [loginId, setLoginId] = useState('')
+
     return(
-        <div>회원가입 페이지</div>
+        <div className={wrapper}>
+            <h1>회원가입</h1>
+
+            <Input
+                value={loginId}
+                onChange={(e) => setLoginId(e.target.value)}
+                label='아이디'
+                placeholder="아이디를 입력해주세요"
+            />
+
+            <Button onClick={() => alert(loginId)}>
+                다음
+            </Button>
+        </div>
     )
 }
