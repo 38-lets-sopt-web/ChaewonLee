911b23f06e461fcb7f3b77fa2428acb7a4db8370
chaewon41 <chaew0m3x@gmail.com>
2026-05-08 19:52:43 +0900

    feat: 마이페이지 라우팅 추가

--- week4/Homework/week4-homework/src/routes/AppRouter.tsx
+++ week4/Homework/week4-homework/src/routes/AppRouter.tsx
@@ -2,13 +2,19 @@ import { Routes, Route } from 'react-router-dom'
 import Login from '../pages/Login'
 import Signup from '../pages/Signup'
 import MyPage from '../pages/MyPage'
+import MyInfo from '../pages/MyPage/MyInfo'
+import MemberInfo from '../pages/MyPage/MemberInfo'
+
 
 export default function AppRouter () {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/mypage" element={<MyPage />}>
+                <Route index element={<MyInfo />} />
+                <Route path="members" element={<MemberInfo />} />
+            </Route>
         </Routes>
     )
 }
