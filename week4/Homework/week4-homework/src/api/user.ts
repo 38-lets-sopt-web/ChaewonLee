e6d84abef54ea68f60057e8259bb3d604ee856c3
chaewon41 <chaew0m3x@gmail.com>
2026-05-08 20:11:43 +0900

    feat: 유저 목록 조회 API 추가

--- week4/Homework/week4-homework/src/api/user.ts
+++ week4/Homework/week4-homework/src/api/user.ts
@@ -22,3 +22,14 @@ export const updateUserInfo = async (
     const res = await instance.patch(`/api/v1/users/${userId}`, data);
     return res.data;
 };
+
+export const getUserList = async () => {
+    const res = await instance.get("/api/v1/users");
+    const { success, message, data } = res.data;
+
+    if (!success) {
+        throw new Error(message);
+    }
+
+    return data.users;
+};
