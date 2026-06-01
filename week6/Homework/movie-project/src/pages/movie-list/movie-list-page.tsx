6e637dae80f388ab5b8475451e4c933bbd169aea
chaewon41 <chaew0m3x@gmail.com>
2026-06-01 22:41:34 +0900

    feat: 영화 목록 페이지 구현

--- week6/Homework/movie-project/src/pages/movie-list/movie-list-page.tsx
+++ week6/Homework/movie-project/src/pages/movie-list/movie-list-page.tsx
@@ -1,7 +1,32 @@
-const MovieListPage = () => (
-  <main className="px-8 py-10">
-    <h1 className="text-display text-primary-600">Movie Project</h1>
-  </main>
-)
+import { useState } from 'react'
+
+import MovieGrid from './components/movie-grid'
+import RatingFilter, {
+  type RatingFilterValue,
+} from './components/rating-filter'
+import { MOCK_MOVIES } from './mock-movies'
+
+const MovieListPage = () => {
+  const [ratingFilter, setRatingFilter] = useState<RatingFilterValue>(null)
+
+  const filteredMovies =
+    ratingFilter === null
+      ? MOCK_MOVIES
+      : MOCK_MOVIES.filter(
+          (movie) =>
+            movie.voteAverage >= ratingFilter.min &&
+            movie.voteAverage < ratingFilter.max,
+        )
+
+  return (
+    <main className="mx-auto max-w-[1120px] px-8 py-14">
+      <h1 className="text-display text-primary-600">Movie Project</h1>
+
+      <RatingFilter value={ratingFilter} onChange={setRatingFilter} />
+
+      <MovieGrid movies={filteredMovies} />
+    </main>
+  )
+}
 
 export default MovieListPage
