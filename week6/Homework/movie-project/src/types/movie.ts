e81569a5ce77b3c096ca9f7d7ddecc00bcda2978
chaewon41 <chaew0m3x@gmail.com>
2026-06-02 20:32:31 +0900

    feat: 영화 상세 페이지 데이터 타입 추가

--- week6/Homework/movie-project/src/types/movie.ts
+++ week6/Homework/movie-project/src/types/movie.ts
@@ -22,6 +22,59 @@ export interface MovieListResponse {
   total_results: number
 }
 
+export interface MovieGenreResponse {
+  id: number
+  name: string
+}
+
+export interface ProductionCompanyResponse {
+  id: number
+  logo_path: string | null
+  name: string
+  origin_country: string
+}
+
+export interface ProductionCountryResponse {
+  iso_3166_1: string
+  name: string
+}
+
+export interface SpokenLanguageResponse {
+  english_name: string
+  iso_639_1: string
+  name: string
+}
+
+export interface MovieDetailResponse {
+  adult: boolean
+  backdrop_path: string | null
+  belongs_to_collection: unknown | null
+  budget: number
+  genres: MovieGenreResponse[]
+  homepage: string
+  id: number
+  imdb_id: string | null
+  origin_country: string[]
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string | null
+  production_companies: ProductionCompanyResponse[]
+  production_countries: ProductionCountryResponse[]
+  release_date: string
+  revenue: number
+  runtime: number | null
+  softcore?: boolean
+  spoken_languages: SpokenLanguageResponse[]
+  status: string
+  tagline: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
 export interface MovieCardItem {
   id: number
   title: string
@@ -35,3 +88,23 @@ export type MovieRatingFilterValue = {
   min: number
   max: number
 } | null
+
+export interface MovieDetailItem {
+  id: number
+  title: string
+  originalTitle: string
+  originalLanguage: string
+  overview: string
+  posterUrl: string
+  backdropUrl: string
+  releaseDate: string
+  genres: string[]
+  voteAverage: number
+  voteCount: number
+  runtime: number
+  status: string
+  productionCountries: string[]
+  spokenLanguages: string[]
+  budget: number
+  revenue: number
+}
